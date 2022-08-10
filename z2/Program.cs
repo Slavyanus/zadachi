d61addd7119010b46991951fe2da64062dc871e4
«Slavyanus <slavyanus@inbox.ru>
2022-08-10 19:02:24 +0700

    third number

---
+++ z2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введи число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+if (b.Length > 2){
+  Console.WriteLine("Третья цифра " + b[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}

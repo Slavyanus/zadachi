a86a17ffb2fe86c4ff8200dcc3aa5549497bf9f1
«Slavyanus <slavyanus@inbox.ru>
2022-08-10 19:50:12 +0700

    day of the week

---
+++ z3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number == 6 || Number == 7)
+{
+    Console.WriteLine("Этот день выходной");
+}
+else if (Number < 1 || Number > 7)
+{
+    Console.WriteLine("Введено неверное значение");
+}
+else Console.WriteLine("Этот день не выходной");
+
+
